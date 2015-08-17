0ac8150d091ee9c32b09eb06f0dd74b3d42719fd
Michael Newton <miken32@gmail.com>
2015-08-17 15:17:40 -0700

    license it

--- page.dundi.php
+++ page.dundi.php
@@ -1,4 +1,21 @@
 <?php
+/***
+FreePBX DUNDi module - https://github.com/miken32/dundi
+Copyright (C) 2015 Point of Presence Techonologies, Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+***/
 $v["module"] = "dundi";
 
 if (isset($_GET["action"])) {
